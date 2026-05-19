fc37ee49b5f69572261a91119bd723284544524c
shin838 <0501sarah@sookmyung.ac.kr>
2026-05-19 15:24:39 +0900

    dbUtil 수정, main에 db 연결 확인 코드 추가

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,21 @@
 package main;
 
+import java.sql.Connection;
+
+import util.DBUtil;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+            Connection conn =
+                DBUtil.getInstance().getConnection();
+
+            System.out.println("DB 연결 성공");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 	}
 
--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
 	//jdk 11버전부터 mysql-connector-java-8.0.13이상 연결시 useSSL=true로 되어 있어 close 시 오류 발생 
 	//useSSL=false로 설정해야 close시 오류가 발생하지 않는다.
-	static final String URL ="jdbc:mysql://localhost:3306/madangdb?serverTimezone=UTC&useUniCode=yes&characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true";
+	static final String URL ="jdbc:mysql://localhost:3306/playlistdb?serverTimezone=UTC&useUniCode=yes&characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true";
 	static final String DRIVER="com.mysql.cj.jdbc.Driver";
 	static final String ID="ureca";
 	static final String PW="ureca";
